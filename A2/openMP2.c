8d7671fc758164aa9099a66f929d4f5418c0a5ef
Gallabano <103634191+Gallabano@users.noreply.github.com>
2022-10-01 19:41:08 +0200

    Add files via upload

--- A2/openMP2.c
+++ A2/openMP2.c
@@ -23,7 +23,7 @@ static inline void distancesWithinBlock(size_t currentBlockLoad, int** block)
             innerProduct = (currentCoordinates[0] - block[j][0])*(currentCoordinates[0] - block[j][0]) +
             (currentCoordinates[1] - block[j][1]) * (currentCoordinates[1] - block[j][1]) +
             (currentCoordinates[2] - block[j][2]) * (currentCoordinates[2] - block[j][2]);
-            totalDistance = (int)(sqrt((double)innerProduct)/10 + 0.5);
+            totalDistance = (int)(sqrtf(innerProduct)/10 + 0.5);
             
             count[totalDistance] += 1;
             
@@ -42,7 +42,7 @@ static inline void distancesBetweenBlocks(size_t currentBlockLoad, int** block,i
         (previousBlockValues[1] - block[i][1]) * (previousBlockValues[1] - block[i][1]) +
         (previousBlockValues[2] - block[i][2]) * (previousBlockValues[2] - block[i][2]);
 
-        totalDistance = (int)(sqrt((double)innerProduct)/10 + 0.5);
+        totalDistance = (int)(sqrtf(innerProduct)/10 + 0.5);
         count[totalDistance] += 1;
     }
 }
@@ -152,13 +152,10 @@ int main()
         free(block);
     } //End of calculating all blocks
     fclose(file);
-    
-    size_t test = 0;
+
     for(size_t i = 0; i < 3465; ++i)
     {   
-        test += count[i];
         printf("%.2f\t%ld\n", (double)i/100, count[i]);
     }
-    printf("Number of distances: %ld\n", test);
-    
+
 }
